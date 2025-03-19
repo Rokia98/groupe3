c2752592ae690a6cfc71decbf0b14a159440ba54
Rokia98 <crokia042@gmail.com>
2025-03-19 15:48:24 +0100

    Ajout du logo et mise à jour du catalogue des audios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
- /** @type {import('tailwindcss').Config} */
- export default {
-  content: ["./src/**/*.{html,js}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"], // 🔥 Adapte selon ton projet
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
